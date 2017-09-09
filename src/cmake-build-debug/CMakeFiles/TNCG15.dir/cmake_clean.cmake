769349d6eddd2d3c0053ac61ecdeedd6bbc55769
Jakob Bertlin <Jakob Bertlin>
2017-09-09 12:59:36 +0200

    Started up project with glm

---
+++ src/cmake-build-debug/CMakeFiles/TNCG15.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TNCG15.dir/main.cpp.obj"
+  "TNCG15.pdb"
+  "TNCG15.exe"
+  "TNCG15.exe.manifest"
+  "libTNCG15.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TNCG15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
