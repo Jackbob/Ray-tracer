2c6625d8adf157bb591b32b92a15c22eb3047480
Jackbob <jakobbertlin@outlook.com>
2017-09-13 15:29:27 +0200

    added triangle functions

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -5,17 +5,28 @@
 #include "Triangle.h"
 
 
-Triangle::Triangle(glm::vec4 v1, glm::vec4 v2, glm::vec4 v3, glm::dvec3 c):
-vertex1{v1}, vertex2{v2}, vertex3{v3}, color{c}, getNormal(v1,v2,v3)
+Triangle::Triangle(glm::vec4 &v1, glm::vec4 &v2, glm::vec4 &v3, glm::dvec3 &c):
+vertex1{v1}, vertex2{v2}, vertex3{v3}, color{c}, normal{getNormal(v1,v2,v3)}
 {
 
 
 
 }
 
-glm::dvec4 Triangle::rayIntersection(ray arg)
+glm::vec4 Triangle::rayIntersection(ray rayarg)
 {
-    //TODO
+    if(glm::dot(normal, glm::vec3(rayarg.endPoint - rayarg.startPoint)) < FLT_EPSILON)
+        return glm::vec4(0.0f, 0.0f, 0.0f, -1.0f);
+
+    float D = glm::dot(normal, glm::vec3(vertex1));
+    float t = - (glm::dot(normal, glm::vec3(rayarg.startPoint)) + D) / glm::dot(normal, glm::vec3(rayarg.endPoint));
+    if(t < FLT_EPSILON)
+        return glm::vec4(0.0f, 0.0f, 0.0f, -1.0f);
+
+    glm::vec4 intersectPoint = rayarg.startPoint + t * rayarg.endPoint;
+    intersectPoint.w = 1.0f;
+
+    return intersectPoint;
 }
 
 glm::vec3 Triangle::getNormal(glm::vec4 v1, glm::vec4 v2, glm::vec4 v3) {
@@ -24,7 +35,5 @@ glm::vec3 Triangle::getNormal(glm::vec4 v1, glm::vec4 v2, glm::vec4 v3) {
     glm::vec3 b = glm::vec3(v2.x, v2.y, v2.z);
     glm::vec3 c = glm::vec3(v3.x, v3.y, v3.z);
 
-
-    normal = glm::normalize(glm::cross(c-a, b-a));
-
+    return glm::normalize(glm::cross(c-a, b-a));
 }
